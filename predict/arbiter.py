89b40dba855a83836779ef01bc182c0484595a67
Vidhya Ramaswamy <vramaswamy@nat-oitwireless-inside-vapornet100-a-21790.Princeton.EDU>
2018-01-08 13:25:30 -0500

    Modified predict - added per interval stats

--- predict/arbiter.py
+++ predict/arbiter.py
@@ -174,44 +174,57 @@ def predict_arbiter_iter(backlog_predictQ, backlog_actualQ, usage, fairness_arra
 	return backlog_predictQ, backlog_actualQ, admitted_array, admitted_queue_size, saved_idx, fairness_array, usage, pred_num
 
 def predict_arbiter(request_objs, timeslot, num_nodes, interval, scale):
-	current_time=0
-	fairness_array = [0]*(num_nodes+1)*(num_nodes+1)
-	usage= [0] *(num_nodes+1)*(num_nodes+1)
-	pred_demand = predictor_core(usage, scale, num_nodes, interval)
-	predQ = make_request_queue(pred_demand, current_time, num_nodes)
-	pred_num=0
+    current_time=0
+    fairness_array = [0]*(num_nodes+1)*(num_nodes+1)
+    usage= [0] *(num_nodes+1)*(num_nodes+1)
+    pred_demand = predictor_core(usage, scale, num_nodes, interval)
+    predQ = make_request_queue(pred_demand, current_time, num_nodes)
+    pred_num=0
 	#print predQ.qsize(), pred_demand[12]
 	
-	backlog_predictQ, backlog_actualQ, admit_array, admit_size, saved_idx, fairness_array, usage, pred_num = predict_arbiter_iter(predQ, Queue(), usage, fairness_array, timeslot, current_time, num_nodes,  0, request_objs, [0]*(num_nodes+1)*(num_nodes+1), pred_num )
+    backlog_predictQ, backlog_actualQ, admit_array, admit_size, saved_idx, fairness_array, usage, pred_num = predict_arbiter_iter(predQ, Queue(), usage, fairness_array, timeslot, current_time, num_nodes,  0, request_objs, [0]*(num_nodes+1)*(num_nodes+1), pred_num )
 
-	#print backlog_predictQ.qsize(), backlog_actualQ.qsize(), admit_size
+    #print backlog_predictQ.qsize(), backlog_actualQ.qsize(), admit_size
 	
-	iter_cnt = 1
-	current_time = 1
-	while True:
-		if iter_cnt%interval ==0:
-			pred_demand = predictor_core(usage, scale, num_nodes, interval)
-			backlog_predictQ = make_request_queue(pred_demand, current_time, num_nodes)
+    iter_cnt = 1
+    current_time = 1    
+    while True:
+        if iter_cnt%interval ==0:
+            sum_predict = 0
+            sum_request = 0
+            extra_pred = 0
+            for i in range(num_nodes+1):
+                for j in range(num_nodes+1):
+                    sum_request+=usage[i*(num_nodes+1)+j]
+                    if (usage[i*(num_nodes+1)+j] <= pred_demand[i*(num_nodes+1)+j]):
+                        sum_predict+= usage[i*(num_nodes+1)+j]
+                        extra_pred+= (pred_demand[i*(num_nodes+1)+j] - usage[i*(num_nodes+1)+j])
+                    else:
+                        sum_predict+= pred_demand[i*(num_nodes+1)+j]
+            
+            print "Predicted/Actual: ", sum_predict, "/", sum_request, "Extra: ", extra_pred
+            pred_demand = predictor_core(usage, scale, num_nodes, interval)
+            backlog_predictQ = make_request_queue(pred_demand, current_time, num_nodes)
 		
-		backlog_predictQ, backlog_actualQ, admit_array, admit_size, saved_idx, fairness_array, usage, pred_num = predict_arbiter_iter(backlog_predictQ, backlog_actualQ, usage, fairness_array, timeslot, current_time, num_nodes, saved_idx, request_objs, admit_array, pred_num)
+        backlog_predictQ, backlog_actualQ, admit_array, admit_size, saved_idx, fairness_array, usage, pred_num = predict_arbiter_iter(backlog_predictQ, backlog_actualQ, usage, fairness_array, timeslot, current_time, num_nodes, saved_idx, request_objs, admit_array, pred_num)
 		#print backlog_predictQ.qsize(), backlog_actualQ.qsize(), admit_size
 		
-		iter_cnt+=1
-		current_time+=1
-		if (backlog_actualQ.empty() and saved_idx== len(request_objs)):
-			break
+        iter_cnt+=1
+        current_time+=1
+        if (backlog_actualQ.empty() and saved_idx== len(request_objs)):
+            break
 
 
-	print "Iterations: ", iter_cnt
-	print "PredictedL ", pred_num	
+    print "Iterations: ", iter_cnt
+    # print "PredictedL ", pred_num	
 
 
 def main():
-    max_requests = 100
+    max_requests = 1000
     num_nodes = 16
     mean_arrival_rate = 20
     mean_size = 1 
-    mean_pred_delay = 10
+    mean_pred_delay = 1
     timeslot = 1
 
     request_objs, request_list = generate_input(max_requests, num_nodes, mean_arrival_rate, mean_size, mean_pred_delay, timeslot)
@@ -224,7 +237,7 @@ def main():
     request_sorted_objs = convert_to_request_objects(request_sorted_list)
     priority_arbiter(request_sorted_objs, timeslot, num_nodes)
 
-    predict_arbiter(request_objs, timeslot,num_nodes, 10, 1.5)
+    predict_arbiter(request_objs, timeslot,num_nodes, 10, 1.0)
 
 if __name__=="__main__":
     main()
